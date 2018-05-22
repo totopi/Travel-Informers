2b0d9866340148b74f919b14ed56156f43422a0d
Corey Clippinger <coreyclip@gmail.com>
2018-05-21 22:08:48 -0700

    added city abbreviation
    so literally you just have to run this script and it should make the city_attributes table. aka the one with all the lat/long values and city abbreviations. The abbr column could be used as a means of seperating the user interface layer from the business logic, if I were able to then go forth and change all the table columns to just be the abbreviations.

--- weather_city_attributes.sql
+++ weather_city_attributes.sql
@@ -4,6 +4,9 @@
 -- ------------------------------------------------------
 -- Server version	5.7.21-log
 
+USE `x5t20wxo8nmqqxfz`;
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -19,12 +22,12 @@
 -- Table structure for table `city_attributes`
 --
 
-DROP TABLE IF EXISTS `city_attributes`;
+DROP TABLE IF EXISTS city_attributes;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `city_attributes` (
-  `city_id` bigint(20) AUTO_INCREMENT,
   `city` text,
+  `abbr` text,
   `country` text,
   `latitude` double DEFAULT NULL,
   `longitude` double DEFAULT NULL
@@ -37,7 +40,7 @@ CREATE TABLE `city_attributes` (
 
 LOCK TABLES `city_attributes` WRITE;
 /*!40000 ALTER TABLE `city_attributes` DISABLE KEYS */;
-INSERT INTO `city_attributes` VALUES ('Vancouver','Canada',49.24966,-123.119339),('Portland','United States',45.523449,-122.676208),('San Francisco','United States',37.774929,-122.419418),('Seattle','United States',47.606209,-122.332069),('LosAngeles','United States',34.052231,-118.243683),('SanDiego','United States',32.715328,-117.157257),('LasVegas','United States',36.174969,-115.137222),('Phoenix','United States',33.44838,-112.074043),('Albuquerque','United States',35.084492,-106.651138),('Denver','United States',39.739151,-104.984703),('SanAntonio','United States',29.42412,-98.493629),('Dallas','United States',32.783058,-96.806671),('Houston','United States',29.763281,-95.363274),('KansasCity','United States',39.099731,-94.578568),('Minneapolis','United States',44.979969,-93.26384),('SaintLouis','United States',38.62727,-90.197891),('Chicago','United States',41.850029,-87.650047),('Nashville','United States',36.16589,-86.784439),('Indianapolis','United States',39.768379,-86.158043),('Atlanta','United States',33.749001,-84.387978),('Detroit','United States',42.331429,-83.045753),('Jacksonville','United States',30.33218,-81.655647),('Charlotte','United States',35.227089,-80.843132),('Miami','United States',25.774269,-80.193657),('Pittsburgh','United States',40.44062,-79.995888),('Toronto','Canada',43.700111,-79.416298),('Philadelphia','United States',39.952339,-75.163788),('New York','United States',40.714272,-74.005966),('Montreal','Canada',45.508839,-73.587807),('Boston','United States',42.358429,-71.059769),('Beersheba','Israel',31.25181,34.791302),('TelAviv','Israel',32.083328,34.799999),('Eilat','Israel',29.55805,34.948212),('Haifa','Israel',32.815559,34.98917),('Nahariyya','Israel',33.005859,35.09409),('Jerusalem','Israel',31.769039,35.216331);
+INSERT INTO `city_attributes` VALUES ('Vancouver', 'van','Canada',49.24966,-123.119339),('Portland','por','United States',45.523449,-122.676208),('San Francisco','sfran','United States',37.774929,-122.419418),('Seattle','sea','United States',47.606209,-122.332069),('Los Angeles','United States',34.052231,-118.243683),('SanDiego','United States',32.715328,-117.157257),('Las Vegas','laveg','United States',36.174969,-115.137222),('Phoenix','pho','United States',33.44838,-112.074043),('Albuquerque','alb','United States',35.084492,-106.651138),('Denver','den','United States',39.739151,-104.984703),('SanAntonio','sant','United States',29.42412,-98.493629),('Dallas','dal','United States',32.783058,-96.806671),('Houston','hou','United States',29.763281,-95.363274),('Kansas City','ksc','United States',39.099731,-94.578568),('Minneapolis','min','United States',44.979969,-93.26384),('Saint Louis','stlo','United States',38.62727,-90.197891),('Chicago','chi','United States',41.850029,-87.650047),('Nashville','nash','United States',36.16589,-86.784439),('Indianapolis','ind','United States',39.768379,-86.158043),('Atlanta','atl','United States',33.749001,-84.387978),('Detroit','det','United States',42.331429,-83.045753),('Jacksonville','United States',30.33218,-81.655647),('Charlotte','char','United States',35.227089,-80.843132),('Miami','mai','United States',25.774269,-80.193657),('Pittsburgh','pit','United States',40.44062,-79.995888),('Toronto','tor','Canada',43.700111,-79.416298),('Philadelphia','phi','United States',39.952339,-75.163788),('New York','nyc','United States',40.714272,-74.005966),('Montreal','mon','Canada',45.508839,-73.587807),('Boston','bos','United States',42.358429,-71.059769),('Beersheba','bee','Israel',31.25181,34.791302),('Tel Aviv','tel','Israel',32.083328,34.799999),('Eilat','eil','Israel',29.55805,34.948212),('Haifa','hai','Israel',32.815559,34.98917),('Nahariyya','nah','Israel',33.005859,35.09409),('Jerusalem','jer','Israel',31.769039,35.216331);
 /*!40000 ALTER TABLE `city_attributes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
