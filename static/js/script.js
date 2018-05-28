651ba89fcd30f07f34c3a7d646710419eac21984
Kevin McNalley <kevin.c.mcnalley@gmail.com>
2018-05-28 13:15:26 -0700

    Working on dynamic graphs

--- static/js/script.js
+++ static/js/script.js
@@ -1,46 +1,57 @@
+// All the scripts!
 
-
-d3.json("/city", function(data) {
-    makeMap(data);
+// Use the route /city to populate our dropdown menu for cities
+let citySelect = document.querySelector("#citySelect");
+d3.json("/city", function(error, cities) {
+    console.log(cities);
+    if (error) throw error;
+    let longness = cities.length;
+    for (let i = 0; i < longness; i++) {
+        if (cities[i]["country"] === "United States") {
+            let city = d3.select("#selCity").append("option").attr("value", cities[i]["city_name"]).text(cities[i]["city_name"]);
+        }//citySelect.append(city);
+    }
 })
 
-function makeMap(data) {
-    console.log(data);
+// Initialize our graphs
+function initGraphs() {
+    let url = "/Portland/05/temp"
 
-    let markers = [];
-    for (let i = 0, ii = data.length; i < ii; i++) {
-        let datum = data[i];
-        markers.push(
-            L.marker([datum.lat, datum.lon], {
-                draggable: false,
-                title: datum.city_name
-            }).bindPopup(`<strong>${datum.city_name}, ${datum.country}</strong><hr>`)
-        )
-    }
+    d3.json(url, function(json) {
+        
+        // First Graph
+        let layout = {
+            title: "Temperature vs Date", 
+            xaxis: {
+                title: "Dates",
+            },
+            yaxis: {
+                title: "Temperature, Degrees Fahrenheit"
+            }
 
-    let startCoords = [34.052231, -118.243683];
-    let startZoom = 6;
+        };
+        Plotly.newPlot('first_graph', json[0], layout);
 
-    let streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidG90b3BpIiwiYSI6ImNqaDFhaGh6ejAxcW4yeHJ5aDl4bjZ2YngifQ.ssPdnszdafCcNm4753AVJA");
+        // Second Graph 
+        let layout2 = {
+            title: "Temperature vs Number of Airport Delays",
+            xaxis: {
+                title: "Temperature"
+            },
+            yaxis: {
+                title: "Number of Airport Delays"
+            },
+        };
 
-    let darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidG90b3BpIiwiYSI6ImNqaDFhaGh6ejAxcW4yeHJ5aDl4bjZ2YngifQ.ssPdnszdafCcNm4753AVJA");
+        Plotly.newPlot('second_graph', json[1], layout2);
 
-    let cityLayer = L.layerGroup(markers);
-
-    let baseMaps = {
-        "Street Map": streetMap,
-        "Dark Map": darkMap
-    };
-
-    let overlayMaps = {
-        "Cities": cityLayer
-    };
-
-    let myMap = L.map('map', {
-        center: startCoords,
-        zoom: startZoom,
-        layers: [streetMap, cityLayer]
+        // Third Plot
+        let pie_layout = {
+            title: "Frequency of Weather Conditions in Portland",
+            height: 700,
+            width: 800
+        };
+        Plotly.newPlot("third_graph", json[2], pie_layout)
     });
-    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
-
 }
+initGraphs()
